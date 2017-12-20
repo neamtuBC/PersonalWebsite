ca5c381a2d9c00a9e353384a9f9e698d4452a999
Neamtu <neamtu.bc@yahoo.com>
2017-12-20 14:22:43 +0200

    javascript mentoring

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,29 @@
-//$(function(){});
+$(function(){
+  mentoringBubbleClick();
+});
+
+function mentoringBubbleClick() {
+  $('.face').on('click',function() {
+
+    var $this = $(this)
+        faceTop = $this.position().top,
+        vertMath = -1 * (faceTop - 200);
+
+    console.log(vertMath);
+
+    $this.parent().css('top',vertMath +'px')
+
+    $this.addClass('has-bubble-open')
+      .siblings().removeClass('has-bubble-open');
+  });
+
+  //when i click a face
+  //get the distance of the face from its parent
+  //move the whole container up 100 + the count
+  //add the is-open class to the face
+
+}
+
 
 $(window).scroll(function(){
   youtubeVidScroll();
@@ -6,8 +31,5 @@ $(window).scroll(function(){
 
 function youtubeVidScroll() {
   var wScroll = $(window).scrollTop();
-
   $('.video-strip').css('background-position','center -'+wScroll+'px');
-
-
 }
